ffd948cde5428313bea9bd1edebe150b4f274c2c
amulya-vijay <amulyakvijay297@gmail.com>
2021-05-03 19:29:36 +0530

    [Amulya] added subtraction operation

--- arithmetic.sh
+++ arithmetic.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
+#Addition
 read -p "Enter number1 : " num1
 read -p "Enter number2 : " num2
 read -p "Enter number3 : " num3
 sum=&(( num1+num2+num3 ))
 echo "Sum="$sum
+#subtraction
+difference=&(( num1-num2-num3 ))
